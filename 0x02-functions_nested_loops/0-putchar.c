3c71a5119615a5cb2a8753059e03b1502b2a3891
oluwafolashademi <folashadeadefiranye@gmail.com>
2022-10-19 17:31:54 -0700

    task 0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include  <unistd.h>
+
+/**
+ * main - write a program that start with _putchar and end with newline.
+ *
+ * that returns: 0
+ *
+ * Written by Folashade
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
