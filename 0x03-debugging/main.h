cd4278697ac0b63c8038a15e8892c23a46e83091
oluwafolashademi <folashadeadefiranye@gmail.com>
2022-10-20 11:38:39 -0700

    task 0 multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,9 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remainig_days(int month, int day, int year);
+int convert_day(int mont, int day);
+void print_times_table(int n);
+
 #endif
