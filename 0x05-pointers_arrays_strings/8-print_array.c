c3fe18b62f4113bc1d25d9a88ba4bf0e59909777
oluwafolashademi <folashadeadefiranye@gmail.com>
2022-10-24 16:30:50 -0700

    task 8 print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - To rint an element of array
+ * @n: number of value to be printed
+ * @a: array
+ */
+
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			print(",");
+	}
+
+	printf("\n");
+}
