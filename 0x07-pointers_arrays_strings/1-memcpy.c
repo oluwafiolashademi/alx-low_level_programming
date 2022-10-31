d2ad64308e4cce35aae400fc475e0f793ab776f7
oluwafolashademi <folashadeadefiranye@gmail.com>
2022-10-31 06:44:44 -0700

    task 1 memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,13 @@
 
 void *_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	char *ptr = dest;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	while (n-- > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++
+	}
 	return (dest);
 }
