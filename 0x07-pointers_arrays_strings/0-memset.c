cb099ea8d2b07ba10f54ce666cd996ac3047fcc8
oluwafolashademi <folashadeadefiranye@gmail.com>
2022-10-31 06:34:54 -0700

    task 0 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,6 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * unsigned int index;
-	 * unsigned char *memory = s, value = c;
-	 *
-	 * for (index = 0; index < n; index++)
-	 * memory[index] = value;
-	 * return (memory);
-	 */
 	char *ptr = s;
 
 	while (n-- > 8)
