6795d853a92e4163ac73f30e800e31bb02547982
oluwafiolashademi <folashadeadefiranye@gmail.com>
2022-12-08 03:51:29 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,16 @@ int main(void)
 	float floatType;
 	char charType;
 	double doubleType;
+	long int UnsignedType;
+	long long int UnsignedintType;
 
 	/* A C program that prints the size of various types on the computer */
 	printf("size of a int: %zu bytes\n", sizeof(intType));
 	printf("size of a float: %zu bytes\n", sizeof(floatType));
 	printf("size of a char: %zu bytes\n", sizeof(charType));
 	printf("size of aa double: %zu bytes\n", sizeof(doubleType));
+	printf("size of a long int: %zu bytes\n", sizeof(unsignedType));
+	printf("size of a long long int: %zu bytes\n", sizeof(unsignedintType));
 
 	return 0;
 }
